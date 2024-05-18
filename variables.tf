03d52f1b66688ee1e6bf0b62d98be62be7e89872
susmitha <gonepadususmithareddy917@gmail.com>
2024-05-18 11:05:09 +0530

    terraform

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+    type = string
+    default = "ami-090252cbe067a9e58"
+}
+
+variable "security_group_ids" {
+    type = list
+    default = ["sg-0f43d0bd748c5c8ee"] #replace with your SG ID.
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+    type = string
+}
+
+variable "tags" {
+    type = map
+    default = {} # this means empty, so not mandatory
+}
